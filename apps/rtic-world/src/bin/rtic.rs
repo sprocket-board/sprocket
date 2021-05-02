fba3ccef8b2473af3d4096a2e7f2b63e05f8b883
James Munns <james.munns@ferrous-systems.com>
2021-05-02 02:00:43 +0200

    Implement message sending between tasks

--- apps/rtic-world/src/bin/rtic.rs
+++ apps/rtic-world/src/bin/rtic.rs
@@ -1,25 +1,28 @@
 #![no_main]
 #![no_std]
 
-use cassette::{yield_now, Cassette};
-use core::future::Future;
-use core::pin::Pin;
-use rtic::app;
 use rtic_world as _; // global logger + panicking-behavior + memory layout
-use sprocket_bsp::Sprocket;
-use static_alloc::Bump;
-use sprocket_bsp::hal::exti::Event;
-use sprocket_bsp::hal::gpio::SignalEdge;
-use sprocket_bsp::hal::exti::ExtiExt;
-use sprocket_bsp::hal::{
-    gpio::gpioa::PA0,
-    gpio::gpioc::PC14,
-    gpio::Output,
-    gpio::PushPull,
-    gpio::Input,
-    gpio::Floating,
+
+use core::{
+    future::Future,
+    pin::Pin,
+    task::Poll,
 };
-use sprocket_bsp::hal::prelude::*;
+use rtic::app;
+
+use cassette::{yield_now, Cassette, futures::poll_fn};
+use heapless::{
+    i,
+    spsc::{Queue, Consumer, Producer},
+    consts,
+};
+use sprocket_bsp::{
+    hal::exti::{Event, ExtiExt},
+    hal::gpio::{gpioa::PA0, gpiob::PB8, Output, PushPull, SignalEdge},
+    hal::prelude::*,
+    Sprocket,
+};
+use static_alloc::Bump;
 
 static A: Bump<[u8; 1024]> = Bump::uninit();
 type StaticCassette<O> = Cassette<Pin<&'static mut (dyn Future<Output = O> + Send)>>;
@@ -50,32 +53,42 @@ where
 const APP: () = {
     struct Resources {
         stepper_cas: StaticCassette<()>,
+        main_cas: StaticCassette<()>,
         exti: sprocket_bsp::hal::stm32::EXTI,
     }
 
     #[init]
-    fn init(ctx: init::Context) -> init::LateResources {
+    fn init(_ctx: init::Context) -> init::LateResources {
+        static mut BTN_TO_MAIN: Queue<u32, consts::U16> = Queue(i::Queue::new());
+
         defmt::info!("Hello, world!");
         let board = defmt::unwrap!(Sprocket::new());
 
+        let (pro, con) = BTN_TO_MAIN.split();
+
         let button_stepper = defmt::unwrap!(caser(
             StepdownButton {
-                count: 5,
+                count: 8,
                 led1: board.led1,
+                ttl_ct: 0,
+                tx: pro,
             },
             StepdownButton::entry,
         ));
 
+        let main_stepper = defmt::unwrap!(caser(
+            MainHandler { rx: AsyncConsumer::new(con), led2: board.led2 },
+            MainHandler::entry,
+        ));
+
         let mut exti = board.exti;
 
-        board.button1.listen(
-            SignalEdge::Falling,
-            &mut exti
-        );
+        board.button1.listen(SignalEdge::Falling, &mut exti);
 
         init::LateResources {
             stepper_cas: button_stepper,
             exti,
+            main_cas: main_stepper,
         }
     }
 
@@ -84,25 +97,22 @@ const APP: () = {
         let exti = ctx.resources.exti;
         let cas = ctx.resources.stepper_cas;
 
-        if exti.is_pending(
-            Event::GPIO14,
-            SignalEdge::Falling,
-        ) {
+        if exti.is_pending(Event::GPIO14, SignalEdge::Falling) {
             exti.unpend(Event::GPIO14);
             match cas.poll_on() {
                 Some(_) => {
-                    defmt::info!("Oops.");
+                    defmt::info!("Done!");
                     rtic_world::exit()
-                },
+                }
                 None => (),
             }
         }
     }
 
-    #[idle]
-    fn idle(_ctx: idle::Context) -> ! {
+    #[idle(resources = [main_cas])]
+    fn idle(ctx: idle::Context) -> ! {
         loop {
-            cortex_m::asm::nop();
+            let _ = ctx.resources.main_cas.poll_on();
         }
     }
 };
@@ -110,16 +120,64 @@ const APP: () = {
 struct StepdownButton {
     count: usize,
     led1: PA0<Output<PushPull>>,
+    tx: Producer<'static, u32, consts::U16>,
+    ttl_ct: u32,
+}
+
+struct MainHandler {
+    rx: AsyncConsumer,
+    led2: PB8<Output<PushPull>>,
+}
+
+impl MainHandler {
+    async fn entry(&mut self) {
+        loop {
+            let msg = self.rx.async_receive().await;
+            defmt::info!("count_a: {=u32}", msg);
+            self.led2.set_low().ok();
+
+            let msg = self.rx.async_receive().await;
+            defmt::info!("count_b: {=u32}", msg);
+            self.led2.set_high().ok();
+        }
+    }
 }
 
 impl StepdownButton {
     async fn entry(&mut self) {
-        // TODO: Should be in a loop {}
-        for _ in 0..self.count {
-            self.led1.set_low().ok();
-            yield_now().await;
-            self.led1.set_high().ok();
-            yield_now().await;
+        loop {
+            for _ in 0..(self.count + 1 / 2) {
+                self.led1.set_low().ok();
+                yield_now().await;
+                self.led1.set_high().ok();
+                yield_now().await;
+                self.ttl_ct = self.ttl_ct.wrapping_add(1);
+
+                // ding
+                defmt::unwrap!(self.tx.enqueue(self.ttl_ct));
+            }
         }
+    }
+}
+
+struct AsyncConsumer {
+    cons: Consumer<'static, u32, consts::U16>,
+}
+
+impl AsyncConsumer {
+    fn new(cons: Consumer<'static, u32, consts::U16>) -> Self {
+        Self {
+            cons,
+        }
+    }
+
+    fn async_receive(&mut self) -> impl Future<Output=u32> + '_ {
+        poll_fn(move |_| {
+            if let Some(msg) = self.cons.dequeue() {
+                Poll::Ready(msg)
+            } else {
+                Poll::Pending
+            }
+        })
     }
 }
